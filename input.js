8567c2a28d0838c0561c30282d289d5ff882dbf8
vagrant <aisle.github@gmail.com>
2021-11-05 05:48:41 +0000

    Implemented input function

---
+++ input.js
@@ -0,0 +1,20 @@
+const { stdin } = require('process');
+
+const handleUserInput = function(key) {
+    if (key === '\u0003') {
+        process.exit();
+      }
+}
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on('data', handleUserInput);
+    return stdin;
+  };
+
+  //stdin.on('data', handleUserInput);
+
+  module.exports = setupInput;
