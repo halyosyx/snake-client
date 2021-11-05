36d636eb930dfbc2864aea9d403cd5b3c380c9f8
vagrant <aisle.github@gmail.com>
2021-11-05 05:23:21 +0000

    Implemented setupInput and handUserINput

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
+const { stdin } = require('process');
 const client = require('./client.js');
 
+const handUserInput = function() {
+    
+}
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on('data', handleUserInput);
+    return stdin;
+  };
+
+  stdin.on('data', handUserInput);
 
 client();
