f5df5498b47396715ef24f7399eadc5099d57250
vagrant <aisle.github@gmail.com>
2021-11-06 17:49:26 +0000

    Implemented constants.js and modified clinet.js and input.js

--- input.js
+++ input.js
@@ -1,19 +1,59 @@
 const { stdin } = require('process');
+let connection;
 
-const handleUserInput = function(key) {
-    if (key === '\u0003') {
-        process.exit();
-      }
-}
-
-const setupInput = function () {
+const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
-    stdin.on('data', handleUserInput);
+    stdin.on('data', key => {
+      handleUserInput(key);
+    });
     return stdin;
   };
+
+  let clear;
+  
+  const handleUserInput = function(key) {
+    
+    const interval = function (wkey){
+      clear = setInterval(() => {
+        connection.write(wkey);
+      }, 100);
+    };
+
+    if (key === '\u0003') {
+        process.exit();
+      }
+    
+    if (key === 'w') {
+      clearInterval(clear);
+      interval("Move: up");   
+    }
+    if (key === 's') {
+      clearInterval(clear);
+      interval("Move: down");
+    }
+    if (key === 'a') {
+      clearInterval(clear);
+      interval("Move: left");
+    }
+    if (key === 'd') {
+      clearInterval(clear);
+      interval("Move: right");
+    }
+
+    //messages
+
+    if (key === 'l') {
+      connection.write('Say: Greetings friend')
+    }
+    if (key === 'k') {
+      connection.write('Say: Time to Eat!')
+    }
+
+};
 
   //stdin.on('data', handleUserInput);
 
--- play.js
+++ play.js
@@ -1,19 +1,5 @@
-const { stdin } = require('process');
-const client = require('./client.js');
+const setupInput = require('./input');
+const connect = require('./client.js');
 
-const handUserInput = function() {
-    
-}
-
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on('data', handleUserInput);
-    return stdin;
-  };
-
-  stdin.on('data', handUserInput);
-
-client();
+console.log('Connecting...')
+setupInput(connect());
